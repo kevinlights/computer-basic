ab88be9f5c412ca09085a7161e91c1b4dda9e5f4
kevinlights <guangjinhai@163.com>
2019-04-22 22:11:21 +0800

    init

---
+++ 计算机如何启动.mm
@@ -0,0 +1,43 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1555941225442" ID="ID_176003648" MODIFIED="1555941740169" STYLE="bubble" TEXT="&#x8ba1;&#x7b97;&#x673a;&#x5982;&#x4f55;&#x542f;&#x52a8;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      bootstrap
+    </p>
+    <p>
+      &#25289;&#38795;&#24102;&#25226;&#33258;&#24049;&#25289;&#36215;&#26469;
+    </p>
+    <p>
+      &#25226;&#19968;&#23567;&#27573;&#31243;&#24207;&#35013;&#36827;&#20869;&#23384;
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1555941546474" ID="ID_1950897127" MODIFIED="1555941740169" POSITION="right" TEXT="BIOS">
+<node CREATED="1555941807714" ID="ID_494596189" MODIFIED="1555941811860" TEXT="ROM">
+<node CREATED="1555941812786" ID="ID_1141235783" MODIFIED="1555941844357" TEXT="read-only memory"/>
+</node>
+<node CREATED="1555941847505" ID="ID_1241755204" MODIFIED="1555941861299" TEXT="basic input/output system"/>
+<node CREATED="1555941884377" ID="ID_1122716706" MODIFIED="1555941894229" TEXT="POST">
+<node CREATED="1555941894229" ID="ID_494832344" MODIFIED="1555941923863" TEXT="Power-On Self-Test"/>
+<node CREATED="1555941964488" ID="ID_1986829297" MODIFIED="1555941969868" TEXT="boot sequence">
+<node CREATED="1555941987128" ID="ID_1178062994" MODIFIED="1555942011144" TEXT="&#x4e0b;&#x4e00;&#x4e2a;&#x9636;&#x6bb5;&#x7684;&#x542f;&#x52a8;&#x7a0b;&#x5e8f;&#x5177;&#x4f53;&#x653e;&#x5728;&#x54ea;&#x4e2a;&#x50a8;&#x5b58;&#x8bbe;&#x5907;"/>
+<node CREATED="1555942016925" ID="ID_1684193578" MODIFIED="1555942024379" TEXT="BIOS&#x91cc;&#x9762;&#x7684;&#x542f;&#x52a8;&#x987a;&#x5e8f;"/>
+</node>
+</node>
+</node>
+<node CREATED="1555941668057" ID="ID_1466015359" MODIFIED="1555941740169" POSITION="left" TEXT="boot">
+<node CREATED="1555941673353" ID="ID_1448144402" MODIFIED="1555941740169" TEXT="bootstrap">
+<node CREATED="1555941681254" ID="ID_657358551" MODIFIED="1555941740169" TEXT="&#x62c9;&#x978b;&#x5e26;&#x628a;&#x81ea;&#x5df1;&#x62c9;&#x8d77;&#x6765;">
+<node CREATED="1555941691097" ID="ID_1536669458" MODIFIED="1555941740169" TEXT="&#x5148;&#x8fd0;&#x884c;&#x7a0b;&#x5e8f;&#xff0c;&#x518d;&#x542f;&#x52a8;&#x8ba1;&#x7b97;&#x673a;"/>
+<node CREATED="1555941717092" ID="ID_1328856172" MODIFIED="1555941740169" TEXT="&#x542f;&#x52a8;&#x8ba1;&#x7b97;&#x673a;&#x540e;&#x624d;&#x80fd;&#x8fd0;&#x884c;&#x7a0b;&#x5e8f;"/>
+</node>
+</node>
+</node>
+</node>
+</map>
